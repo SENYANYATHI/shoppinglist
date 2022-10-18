84d268735c122ea848d19726bd98e24b4c1f694a
unknown <msenyanyathi33@gmail.com>
2022-10-18 15:03:15 +0200

    update

---
+++ src/constants/globalStyles.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  primaryColor: {
+    // color: '#7CCEE0',
+    color:'#5de2ff',
+  },
+  secondaryColor: {
+    backgroundColor: '#4BB7CF',
+  },
+});
